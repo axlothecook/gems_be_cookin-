edec6e518c977f8a3a80ff13df59dae893246137
axlothecook <alliston.ruby123@gmail.com>
2024-10-21 15:31:49 +0200

    uploading files

---
+++ webpack-container/src/index.js
@@ -0,0 +1,10 @@
+import './styles.css';
+
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+
+console.log('i am console logging');
